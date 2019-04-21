5910502430329eb884d146b631a6e442fd82a997
medzahra <meddz213u@gmail.com>
2019-04-21 21:14:40 +0100

    edit App.js

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -16,13 +16,13 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>Hello!!!</Text>
+        <Text style={styles.instructions}>This is my first APP</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
